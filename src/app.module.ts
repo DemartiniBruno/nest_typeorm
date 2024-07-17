f36adfbab7499520b93ad231a2e33952ec18ba73
Bruno Demartini <bruno.r.demartini@gmail.com>
2024-07-17 16:59:26 -0300

    Criado um gateway de comunicação exemplo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { VendasModule } from './vendas/vendas.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, VendasModule],
+  imports: [DatabaseModule, UsersModule, VendasModule, ChatModule],
   controllers: [AppController],
   providers: [AppService,],
+  // imports: [ChatModule],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
